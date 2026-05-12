abcf432610557b7359b75115b4989debebe33e06
editoraky <25002925+editoraky@users.noreply.github.com>
2026-05-12 20:26:15 +0200

    Register Board, Task, Comment and custom UserAdmin in admin

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'fullname', 'is_staff')
+    ordering = ('email',)
+    search_fields = ('email', 'fullname')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('fullname',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'fullname', 'password1', 'password2', 'is_staff', 'is_superuser'),
+        }),
+    )
